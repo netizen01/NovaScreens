d6de8ad713aa2465635b4e68c4bcdfce66fa0769
Joel Stewart <joel@codeinversion.com>
2016-11-18 17:19:53 -0600

    swift3

--- Source/NovaScreens.swift
+++ Source/NovaScreens.swift
@@ -6,38 +6,38 @@ import UIKit
 
 public protocol UIApplicationScreenDelegate: UIApplicationDelegate {
 
-    func application(application: UIApplication, didConnectScreen screen: UIScreen)
-    func application(application: UIApplication, didDisconnectScreen screen: UIScreen)
+    func application(_ application: UIApplication, didConnectScreen screen: UIScreen)
+    func application(_ application: UIApplication, didDisconnectScreen screen: UIScreen)
 
 }
 
 extension UIApplication {
 
     public func startScreenNotifications() {
-        NSOperationQueue.mainQueue().addOperationWithBlock {
-            if UIScreen.screens().count > 1 {
-                for screen in UIScreen.screens() {
-                    if screen !== UIScreen.mainScreen() {
+        OperationQueue.main.addOperation {
+            if UIScreen.screens.count > 1 {
+                for screen in UIScreen.screens {
+                    if screen !== UIScreen.main {
                         self.applicationDidConnectScreen(screen)
                     }
                 }
             }
 
-            let center = NSNotificationCenter.defaultCenter()
+            let center = NotificationCenter.default
             
             // Clear out existing notification observers to prevent multiple entries
-            center.removeObserver(self, name: UIScreenDidConnectNotification, object: nil)
-            center.removeObserver(self, name: UIScreenDidDisconnectNotification, object: nil)
+            center.removeObserver(self, name: NSNotification.Name.UIScreenDidConnect, object: nil)
+            center.removeObserver(self, name: NSNotification.Name.UIScreenDidDisconnect, object: nil)
             
-            center.addObserver(self, selector: #selector(UIApplication.screenDidConnectNotification(_:)), name: UIScreenDidConnectNotification, object: nil)
-            center.addObserver(self, selector: #selector(UIApplication.screenDidDisconnectNotification(_:)), name: UIScreenDidDisconnectNotification, object: nil)
+            center.addObserver(self, selector: #selector(UIApplication.screenDidConnectNotification(_:)), name: NSNotification.Name.UIScreenDidConnect, object: nil)
+            center.addObserver(self, selector: #selector(UIApplication.screenDidDisconnectNotification(_:)), name: NSNotification.Name.UIScreenDidDisconnect, object: nil)
         }
     }
     
     public func stopScreenNotifications() {
-        let center = NSNotificationCenter.defaultCenter()
-        center.removeObserver(self, name: UIScreenDidConnectNotification, object: nil)
-        center.removeObserver(self, name: UIScreenDidDisconnectNotification, object: nil)
+        let center = NotificationCenter.default
+        center.removeObserver(self, name: NSNotification.Name.UIScreenDidConnect, object: nil)
+        center.removeObserver(self, name: NSNotification.Name.UIScreenDidDisconnect, object: nil)
     }
 
 }
@@ -45,25 +45,25 @@ extension UIApplication {
 // Private Extension Functions
 extension UIApplication {
     
-    func screenDidConnectNotification(notification: NSNotification) {
+    func screenDidConnectNotification(_ notification: Notification) {
         if let screen = notification.object as? UIScreen {
             applicationDidConnectScreen(screen)
         }
     }
 
-    func screenDidDisconnectNotification(notification: NSNotification) {
+    func screenDidDisconnectNotification(_ notification: Notification) {
         if let screen = notification.object as? UIScreen {
             applicationDidDisconnectScreen(screen)
         }
     }
 
-    func applicationDidConnectScreen(screen: UIScreen) {
+    func applicationDidConnectScreen(_ screen: UIScreen) {
         if let delegate = delegate as? UIApplicationScreenDelegate {
             delegate.application(self, didConnectScreen: screen)
         }
     }
 
-    func applicationDidDisconnectScreen(screen: UIScreen) {
+    func applicationDidDisconnectScreen(_ screen: UIScreen) {
         if let delegate = delegate as? UIApplicationScreenDelegate {
             delegate.application(self, didDisconnectScreen: screen)
         }
